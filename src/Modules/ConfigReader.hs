7bba8cc28828126756bdf94ac7724a5216719524
Tootooroo <aydenlin@foxmail.com>
2019-09-09 18:58:24 +0800

    ConfigReader update

--- src/Modules/ConfigReader.hs
+++ src/Modules/ConfigReader.hs
@@ -7,14 +7,18 @@ import Text.Parsec.Expr
 import Text.Parsec.Token
 import Text.Parsec.Language
 
+import Data.Map as Map (Map, fromList, empty, lookup, toList)
+import Data.Map.Merge.Strict
 import Data.Maybe
 import Data.List.Split
 
-type Configs = [[String]]
-type Config = [String]
+data Configs = Configs_M { configMap :: Map String Configs }
+              | Configs_L { configList :: [Configs] }
+              | Configs_Str { configVal :: [Char] }
+              | Configs_Empty deriving Show
 
 data Projects_cfg = ProjectsConfig_cfg { projName :: String } deriving Show
-data TestProject_cfg = TestProject_cfg { testName :: String, testContent :: [String]  } deriving Show
+data TestProject_cfg = TestProject_cfg { testContent :: Map String [String] } deriving Show
 data EmailInfo_cfg = EmailInfo_cfg { host :: String, user :: String, pass :: String } deriving Show
 data AdminEmailAddr_cfg = AdminEmailAddr_cfg { adminEmailAddr :: String } deriving Show
 data AcceptApi_cfg = AcceptApi_cfg { a_api :: String } deriving Show
@@ -28,20 +32,28 @@ data ServerInfo_cfg = ServerInfo_cfg { addr :: String, port :: String } deriving
 configFile :: GenParser Char st Configs
 configFile = do
   skipMany configCommentOrBlank
-  sepEndBy configOption (char '\n' >> skipMany configCommentOrBlank)
+  c <- sepEndBy configOption (char '\n' >> skipMany configCommentOrBlank)
+  return $ foldl (\(Configs_M x) (Configs_M y) -> Configs_M $ merge1 x y) (Configs_M empty) c
 
-configCommentOrBlank :: GenParser Char st Config
+  where merge1 m1 m2 = merge
+                 (mapMaybeMissing (\k v -> return v))
+                 (mapMaybeMissing (\k v -> return v))
+                 (zipWithMaybeMatched (\k v1 v2 -> return v1))
+                 m1 m2
+
+
+configCommentOrBlank :: GenParser Char st ()
 configCommentOrBlank = do
   (char '#' >> many (noneOf "\n")) <|> many mySpace
   eol
-  return []
+  return ()
 
-configOption :: GenParser Char st Config
+configOption :: GenParser Char st Configs
 configOption = do
   head <- optHead
   sep  <- optDefSeperate
   def  <- try optDefs <|> optDef
-  return (head:def)
+  return $ Configs_M $ fromList [(head, def)]
 
 optHead :: GenParser Char st String
 optHead = do
@@ -64,46 +76,65 @@ optDefSeperate = do
   skipMany (try mySpace)
   return ":"
 
-optDef :: GenParser Char st Config
+optDef :: GenParser Char st Configs
 optDef = do
   def <- defStmt
   return def
 
-optDefs :: GenParser Char st Config
+optDefs :: GenParser Char st Configs
 optDefs = do
   eol
   optDefPrefix
   x <- defStmt
-  xs <- try optDefs <|> return []
-  return (x ++ xs)
+  xs <- try optDefs <|> return (Configs_L [Configs_Empty])
 
-defStmt :: GenParser Char st Config
+  case x of
+    Configs_L l -> return $ Configs_L $ (configList x) ++ (configList xs)
+    Configs_M m -> return $ Configs_M $ merge1 m (configMap xs)
+  where merge1 m1 m2 = merge
+                       (mapMaybeMissing (\k v -> return v))
+                       (mapMaybeMissing (\k v -> return v))
+                       (zipWithMaybeMatched (\k v1 v2 -> return v1))
+                       m1 m2
+
+defStmt :: GenParser Char st Configs
 defStmt = do
-  ret <- defArray <|> defPair <|> defObj
+  ret <- defArray <|> defPair <|> defObj <|> defMap
   return ret
 
-defObj :: GenParser Char st Config
+defObj :: GenParser Char st Configs
 defObj = do
   str <- myString
-  return (str:[])
+  return $ Configs_Str str
 
-defArray :: GenParser Char st Config
+defArray :: GenParser Char st Configs
 defArray = do
   char '['
   body <- defArray_body
   char ']'
   return body
 
-defArray_body :: GenParser Char st Config
+defMap :: GenParser Char st Configs
+defMap = do
+  char '{'
+  skipMany (try mySpace)
+  key <- myString
+  skipMany (try mySpace)
+  char ':'
+  body <- defArray_body
+  char '}'
+  return $ Configs_M $ fromList [(key, body)]
+
+defArray_body :: GenParser Char st Configs
 defArray_body = do
   skipMany (try mySpace)
 
   str <- myString
 
-  strN <- (skipMany (try mySpace) >> char ',' >> defArray_body) <|> return []
-  return $ str:strN
+  strN <- (skipMany (try mySpace) >> char ',' >> defArray_body) <|> return Configs_Empty
+  return $ Configs_L $ (Configs_Str str):(configList strN)
 
-defPair :: GenParser Char st Config
+defPair :: GenParser Char st Configs
 defPair = do
   char '('
   left <- myString
@@ -112,7 +143,7 @@ defPair = do
   spaces
   right <- myString
   char ')'
-  return ((left ++ " " ++ right):[])
+  return $ Configs_L [Configs_Str left, Configs_Str right]
 
 eol :: GenParser Char st Char
 eol = try (char '\n') <|>
@@ -136,57 +167,30 @@ optDefPrefix = do
 parseConfig :: String -> Either ParseError Configs
 parseConfig input = parse configFile "(unknown)" input
 
-searchConfig :: String -> Configs -> Maybe Config
-searchConfig optKey [] = Nothing
+searchConfig :: String -> Configs -> Maybe Configs
+searchConfig optKey Configs_Empty = Nothing
 searchConfig optKey opts = do
-  let result = filter (\(x:xs) -> x == optKey) $ opts
-  if null result
-    then Nothing
-    else return $ tail . head $ result
+  case Map.lookup optKey map of
+    Nothing -> Nothing
+    Just c -> return c
 
-listProjConfig :: Configs -> Maybe Config
+  where map = configMap opts
+
+listProjConfig :: Configs -> Maybe Projects_cfg
 listProjConfig opts = do
-  if prjs == Nothing
+  if isNothing prjs
     then Nothing
-    else return $ fromJust $ prjs
+    else let prj_ = fromJust prjs
+         in return $ ProjectsConfig_cfg (configVal $ head $ configList prj_)
   where prjs = searchConfig "Projects" opts
 
 isProjExists :: String -> Configs -> Bool
-isProjExists prjName opts = if prjs == Nothing
+isProjExists prjName opts = if isNothing prjs
                                then False
-                               else elem True [ x == prjName | x <- (fromJust $ prjs) ]
+                               else elem True [ (configVal x) == prjName | x <- (configList $ fromJust $ prjs) ]
   where prjs = searchConfig "Projects" opts
 
-windRiverPathConfig :: Configs -> Maybe String
-windRiverPathConfig [] = Nothing
-windRiverPathConfig opts = do
-  path <- searchConfig "WindRiverPath" opts
-  return $ head path
-
-tempDirPathConfig :: Configs -> Maybe String
-tempDirPathConfig [] = Nothing
-tempDirPathConfig opts = do
-  path <- searchConfig "TempDirPath" opts
-  return $ head path
-
-pairValueSearch :: String -> String -> Configs -> Maybe String
-pairValueSearch prjName opt opts = do
-  apis <- let mr = searchConfig opt opts
-          in if mr == Nothing
-                then Just []
-                else mr
-  if apis == []
-     then Nothing
-     else return $ last $ head [ splitOn " " x | x <- apis, isTheApi x ]
-  where isTheApi = (== prjName) . head . splitOn " "
-
-mrAccessApiConfig :: String -> Configs -> Maybe String
-mrAccessApiConfig prjName opts = pairValueSearch prjName "MRAccessApi" opts
-
-sourceUrlConfig :: String -> Configs -> Maybe String
-sourceUrlConfig prjName opts = pairValueSearch prjName "SourceUrl" opts
-
-configRetrive :: Configs -> String -> (Config -> b) -> Maybe b
+configRetrive :: Configs -> String -> (Configs -> b) -> Maybe b
 configRetrive opts optName f = do
   let optVals = searchConfig optName opts
 
@@ -195,28 +199,35 @@ configRetrive opts optName f = do
     else return $ f $ fromJust optVals
 
 mrAcceptApiConfig :: Configs -> Maybe AcceptApi_cfg
-mrAcceptApiConfig opts = configRetrive opts "MRAcceptApi" (\cfg -> AcceptApi_cfg $ head cfg)
+mrAcceptApiConfig opts = configRetrive opts "MRAcceptApi"
+                         (\cfg -> AcceptApi_cfg $ configVal $ head $ configList $ cfg)
 
 mrRebaseApiConfig :: Configs -> Maybe RebaseApi_cfg
-mrRebaseApiConfig opts = configRetrive opts "MRRebaseApi" (\cfg -> RebaseApi_cfg $ head cfg)
+mrRebaseApiConfig opts = configRetrive opts "MRRebaseApi"
+                         (\cfg -> RebaseApi_cfg $ configVal $ head $ configList cfg)
 
 emailInfoGet :: Configs -> Maybe EmailInfo_cfg
 emailInfoGet opts = configRetrive opts "Email"
-  (\cfg -> EmailInfo_cfg (head cfg) (head . tail $ cfg) (last cfg))
+  (\(Configs_L cfg_l) -> EmailInfo_cfg (configVal $ head cfg_l) (configVal $ head . tail $ cfg_l) (configVal $ last cfg_l))
 
 adminEmailGet :: Configs -> Maybe AdminEmailAddr_cfg
-adminEmailGet opts = configRetrive opts "AdminEmail" (\cfg -> AdminEmailAddr_cfg $ head cfg)
+adminEmailGet opts = configRetrive opts "AdminEmail" (\(Configs_L cfg) -> AdminEmailAddr_cfg $ configVal $ head cfg)
 
 testCmdGet :: Configs -> Maybe TestContent_cfg
-testCmdGet opts = configRetrive opts "TestCmd" (\cfg -> TestContent_cfg cfg)
+testCmdGet opts = configRetrive opts "TestCmd" (\(Configs_L cfg) -> TestContent_cfg $ [configVal x | x <- cfg])
 
-testPiecesGet :: Configs -> Maybe TestProject_cfg
-testPiecesGet opts = configRetrive opts "TestProject" (\cfg -> TestProject_cfg (head cfg) (tail cfg))
+testPiecesGet :: String -> Configs -> Maybe TestProject_cfg
+testPiecesGet projName opts = configRetrive opts "TestProject" (getContent)
+  where getContent (Configs_M m) =  TestProject_cfg $ fromList [ (x, cValsToStrs y)| (x, Configs_L y) <- toList m]
+        cValsToStrs cVals = [ configVal cv | cv <- cVals]
 
 databaseGet :: Configs -> Maybe DatabaseInfo_cfg
 databaseGet opts = configRetrive opts "Database"
-  (\cfg -> DatabaseInfo_cfg (head cfg) (head . tail $ cfg) (head . tail . tail $ cfg) (last cfg))
+  (\(Configs_L cfg) -> DatabaseInfo_cfg (configVal $ head cfg)
+                       (configVal $ head . tail $ cfg)
+                       (configVal $ head . tail . tail $ cfg)
+                       (configVal $ last cfg))
 
 serverInfoGet :: Configs -> Maybe ServerInfo_cfg
 serverInfoGet opts = configRetrive opts "ServerAddr"
-  (\cfg -> ServerInfo_cfg (head cfg) (last cfg))
+  (\(Configs_L cfg) -> ServerInfo_cfg (configVal $ head cfg) (configVal $ last cfg))
